db813d819c91a496f1f1624aa334f9f465db2a8f
Chandana Mallu <chaaand2001@gmail.com>
2025-04-20 17:42:46 -0400

    made teh navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,52 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation} from 'react-router-dom';
 import './Navbar.css'; // optional, for your styles
 
 export default function Navbar( { isLoggedIn, onLogout } ) {
+  const { pathname } = useLocation();
+  const isLoginPage = pathname === '/login';
+  const showNav = isLoggedIn && !isLoginPage;
+
+  function closeMenu() {
+    const toggle = document.getElementById('nav-toggle');
+    if (toggle) toggle.checked = false;
+  }
+
   return (
 <nav className="navbar">
       <div className="brand">Fetch</div>
 
-      <ul className="nav-list">
-        {isLoggedIn ? (
-          <>
-            <li><NavLink to="/search">Search</NavLink></li>
-            <li><NavLink to="/matchfound">Favorites</NavLink></li>
+{showNav && (
+        <>
+          <input type="checkbox" id="nav-toggle" className="nav-toggle"/>
+          <label htmlFor="nav-toggle" className="menu-btn">&#9776;</label>
+
+          <ul className="nav-list">
             <li>
-              <button className="logout-button" onClick={onLogout}>
+              <NavLink to="/search" onClick={closeMenu}>
+                Search
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/matchfound" onClick={closeMenu}>
+                Favorites
+              </NavLink>
+            </li>
+            <li>
+              <button
+                className="logout-button"
+                onClick={() => {
+                  onLogout();
+                  closeMenu();
+                }}
+              >
                 Log Out
               </button>
             </li>
-          </>
-        ) : (
-          <span></span>
-        )}
-      </ul>
+          </ul>
+        </>
+      )}
+
     </nav>
   );
 }
